6e7e2a644c04677ec660e942f33ce4e65e70f6a1
tomomiKIDA <kida.biansha@gmail.com>
2020-08-02 07:20:32 +0000

    sum success

--- sum.php
+++ sum.php
@@ -20,23 +20,49 @@ $array = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 echo count($array);
 $array = array(2,5,9,7,3,1,8,6,4);
 arsort($array);
-print_r($array); */
+print_r($array); 
+
+
+1.引数に数値を指定して実行すると、
+数値を2倍にして返す関数を作成してください*/
 $num = 5;
 $result1 = $num * 2;
+echo $result1;
+echo "\n";
 
+/*2.$a と $b を仮引数に持ち、　$a と $b
+を足した結果を返す関数を作成してください。*/
 function f($a,$b){
-	$result2 = $a + $b;
-	return $result2;
+	$sum = $a + $b;
+	return $sum;
 }
+$result = f(8,9);
+echo $result;
+echo "\n";
 
-$arr = array(1, 3, 5 ,7, 9);
-$result3 = array_product($arr);
-
+/*3.$arr という配列の仮引数を持ち、
+数値が入った配列array(1, 3, 5 ,7, 9) 
+を渡すとその要素をすべてかけた結果を返す関数を作成してください。*/
+function all($arr){
+	$result = 1;
+	foreach($arr as $a){
+		$result *= $a;
+	}
+	echo $result;
+}
+all (array(1, 3, 5 ,7, 9) );
+echo "\n";
+/*4.【応用】　次のプログラムは、配列の中で1番大きい値を返す
+max_array という関数を実装しようとしています。
+途中の部分を完成させてください。*/
 function max_array($arr){
  $max_number = $arr[0];
  foreach($arr as $a){
- arsort($arr)
+	if ($max_number < $a){
+		$max_number == $a;
+	}
+	} 
+	 return $max_number;
  }
- return $max_number;
- }
-?>
+ echo max_array (array(20, 5, 8, 2));
+ ?>
